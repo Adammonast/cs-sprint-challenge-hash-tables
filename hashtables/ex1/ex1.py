b9fdafe4a789208e358d1230625bd06014eb0175
Adam <adammonast@Adams-MBP.attlocal.net>
2020-07-10 11:33:54 -0400

    pseudo code written out, have an idea for ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -4,4 +4,15 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     # Your code here
 
+    # todo:
+    # given a package with a weight limit `limit` and a list `weights` of item weights
+    # implement function that finds two items whose SUM of weight EQUALS the weight limit
+    # function returns a tuple of integers ---> (zero, one)
+    # each element represents the item weights of the two packages
+    # The higher valued index should be placed in the `zeroth` index and the smaller index should be placed in the `first` index
+    # If such a pair doesn’t exist: function should return `None`.
+    # linear time - as input increases, runtime/space used will grow at the SAME rate
+
+    # check the length first
+
     return None
